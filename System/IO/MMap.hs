b352c82d98fb1c0c2ea5d77070fb89c6eb903358
none <none>
2008-03-01 12:32:38 -0600

    documentation changes
    darcs-hash:65ac333e9bb88decf33ddbdee8d4a26421c72cab

--- System/IO/MMap.hs
+++ System/IO/MMap.hs
@@ -1,34 +1,42 @@
 {-# OPTIONS_GHC -fglasgow-exts #-}
 
-module System.IO.MMap 
+module System.IO.MMap
 (
      -- $mmap_intro
-     -- * Memory mapped files
+
+     -- * Memory mapped files strict interface
      mmapFilePtr,
      mmapFileForeignPtr,
      mmapFileByteString,
-     
+
+     -- * Memory mapped files lazy interface
+     mmapFilePtrLazy,
+     mmapFileForeignPtrLazy,
+     mmapFileByteStringLazy,
+
+     -- * Mapping mode
      Mode(..)
 )
 where
 
-import System.IO
-import Foreign.Ptr
-import Foreign.C.Types
-import Foreign.C.String
-import Foreign.ForeignPtr
-import Foreign.Concurrent
-import System.IO.Unsafe
-import Data.ByteString.Unsafe
-import Data.Int
-import Control.Monad
-import Control.Exception
-import Data.ByteString
+import System.IO ()
+import Foreign.Ptr (Ptr,FunPtr,nullPtr,plusPtr)
+import Foreign.C.Types (CInt,CLLong)
+import Foreign.C.String (CString,withCString)
+import Foreign.ForeignPtr (ForeignPtr,withForeignPtr,finalizeForeignPtr,newForeignPtr)
+import Foreign.Concurrent (newForeignPtr)
+import System.IO.Unsafe  (unsafePerformIO)
+import qualified Data.ByteString.Unsafe as BS (unsafePackCStringFinalizer)
+import Data.Int (Int64)
+import Control.Monad  (when)
+import Control.Exception   (bracket)
+import qualified Data.ByteString as BS (ByteString)
+import qualified Data.ByteString.Lazy as BSL  (ByteString,fromChunks)
 
 -- $mmap_intro
 --
 -- This module is an interface to mmap(2) system call under POSIX (Unix, Linux,
--- Mac OS X) and CreateFileMapping MapViewOfFile under Windows.
+-- Mac OS X) and CreateFileMapping,MapViewOfFile under Windows.
 --
 -- We can consider mmap as lazy IO pushed into the virtual memory
 -- subsystem.
@@ -71,12 +79,12 @@ data Mode = ReadOnly      -- ^ file is mapped read-only
 -- If mode is 'ReadWrite' or 'WriteCopy', the returned memory region may
 -- be written to with 'Foreign.Storable.poke' and friends.
 --
--- Range specified may be 'Nothing', then whole file is maped. Otherwise
+-- Range specified may be 'Nothing', then whole file is mapped. Otherwise
 -- range should be 'Just (offset,size)' where offsets is the beginning byte
 -- of file region to map and size tells its length. There are no alignment
 -- requirements.
 --
--- If range to map extends beyon end of file, it will be resized accordingly.
+-- If range to map extends beyond end of file, it will be resized accordingly.
 --
 mmapFilePtr :: FilePath                -- ^ name of file to mmap
             -> Mode                    -- ^ access mode
@@ -92,7 +100,7 @@ mmapFilePtr filepath mode offsetsize = do
                 Nothing -> do
                     longsize <- withForeignPtr handle c_system_io_file_size
                     when (longsize > fromIntegral (maxBound :: Int)) $
-                         error ("file is longer (" ++ show longsize ++ ") then maxBound :: Int")
+                         error ("file is longer (" ++ show longsize ++ ") than maxBound::Int")
                     return (0,fromIntegral longsize)
             withForeignPtr handle $ \handle -> do
                 let align = offset `mod` fromIntegral c_system_io_granularity
@@ -120,15 +128,111 @@ mmapFileForeignPtr filepath mode offsetsize = do
 --
 -- Note: this operation may break referential transparency! If
 -- any other process on the system changes the file when it is mapped
--- into Haskell, the contents of your 'ByteString' will change.
+-- into Haskell, the contents of your 'Data.ByteString.ByteString' may change.
 --
 mmapFileByteString :: FilePath                     -- ^ name of file to map
                    -> Maybe (Int64,Int)            -- ^ range to map, maps whole file if Nothing
-                   -> IO ByteString                -- ^ bytestring with file content
+                   -> IO BS.ByteString                -- ^ bytestring with file content
 mmapFileByteString filepath offsetsize = do
     (ptr,finalizer,size) <- mmapFilePtr filepath ReadOnly offsetsize
-    bytestring <- unsafePackCStringFinalizer ptr size finalizer
+    bytestring <- BS.unsafePackCStringFinalizer ptr size finalizer
     return bytestring
+
+-- | The 'mmapFilePtrLazy' function maps a file or device into memory,
+-- returning a list of tripples containing pointer that accesses the mapped file,
+-- the finalizer to run to unmap that region and size of mapped memory.
+--
+-- If the mmap fails for some reason, an error is thrown.
+--
+-- Memory mapped files will behave as if they were read lazily --
+-- pages from the file will be loaded into memory on demand.
+--
+-- The storage manager is used to free the mapped memory. When
+-- the garbage collector notices there are no further references to the
+-- mapped memory, a call to munmap is made. It is not necessary to do
+-- this yourself. In tight memory situations, it may be profitable to
+-- use 'System.Mem.performGC' or 'finalizeForeignPtr' to force an unmap.
+--
+-- File must be created with correct attributes prior to mapping it
+-- into memory.
+--
+-- If mode is 'ReadWrite' or 'WriteCopy', the returned memory region may
+-- be written to with 'Foreign.Storable.poke' and friends.
+--
+-- Range specified may be 'Nothing', then whole file is mapped. Otherwise
+-- range should be 'Just (offset,size)' where offsets is the beginning byte
+-- of file region to map and size tells its length. There are no alignment
+-- requirements.
+--
+-- If range to map extends beyond end of file, it will be resized accordingly.
+--
+mmapFilePtrLazy :: FilePath                -- ^ name of file to mmap
+            -> Mode                        -- ^ access mode
+            -> Maybe (Int64,Int64)         -- ^ range to map, maps whole file if Nothing
+            -> IO [(Ptr a,IO (),Int)]      -- ^ list of pointer, finalizer and size
+mmapFilePtrLazy filepath mode offsetsize = do
+    handle <- mmapFileOpen filepath mode
+    mmap handle
+    where
+        mmap handle = do
+            (offset,size) <- case offsetsize of
+                Just (offset,size) -> return (offset,size)
+                Nothing -> do
+                    longsize <- withForeignPtr handle c_system_io_file_size
+                    return (0,fromIntegral longsize)
+            return $ map (mapChunk handle) (chunks offset size)
+        mapChunk handle (offset,size) = unsafePerformIO $
+            withForeignPtr handle $ \handle -> do
+                let align = offset `mod` fromIntegral c_system_io_granularity
+                    offsetraw = offset - align
+                    sizeraw = size + fromIntegral align
+                ptr <- c_system_io_mmap_mmap handle (fromIntegral $ fromEnum mode) (fromIntegral offsetraw) (fromIntegral sizeraw)
+                when (ptr == nullPtr) $
+                    error "c_system_io_mmap_mmap returned NULL"
+                let finalizer = c_system_io_mmap_munmap ptr (fromIntegral sizeraw)
+                return (ptr `plusPtr` fromIntegral align,finalizer,fromIntegral size)
+
+chunks :: Int64 -> Int64 -> [(Int64,Int)]
+chunks offset size | size <= fromIntegral chunkSize = [(offset,fromIntegral size)]
+                   | otherwise = let offset2 = offset + fromIntegral chunkSize `div` fromIntegral chunkSize * fromIntegral chunkSize
+                                     size2 = fromIntegral (offset2 - offset)
+                                 in (offset,size2) : chunks (offset2) (size-fromIntegral size2)
+
+-- | Maps region of file and returns it as list of 'ForeignPtr's. See 'mmapFilePtr' for details.
+-- Each chunk is mapped in on demand only.
+mmapFileForeignPtrLazy :: FilePath                   -- ^ name of file to map
+                   -> Mode                           -- ^ access mode
+                   -> Maybe (Int64,Int64)            -- ^ range to map, maps whole file if Nothing
+                   -> IO [(ForeignPtr a,Int)]        -- ^ foreign pointer to beginning of region and size
+mmapFileForeignPtrLazy filepath mode offsetsize = do
+    list <- mmapFilePtrLazy filepath mode offsetsize
+    return (map turn list)
+    where
+        turn (ptr,finalizer,size) = unsafePerformIO $ do
+            foreignptr <- Foreign.Concurrent.newForeignPtr ptr finalizer
+            return (foreignptr,size)
+
+-- | Maps region of file and returns it as 'Data.ByteString.Lazy.ByteString'.
+-- File is mapped in in 'ReadOnly' mode. See 'mmapFilePtrLazy' for details.
+-- Chunks are mapped in on demand.
+--
+-- Note: this operation may break referential transparency! If
+-- any other process on the system changes the file when it is mapped
+-- into Haskell, the contents of your 'Data.ByteString.Lazy.ByteString' may change.
+--
+mmapFileByteStringLazy :: FilePath                     -- ^ name of file to map
+                       -> Maybe (Int64,Int64)          -- ^ range to map, maps whole file if Nothing
+                       -> IO BSL.ByteString            -- ^ bytestring with file content
+mmapFileByteStringLazy filepath offsetsize = do
+    list <- mmapFilePtrLazy filepath ReadOnly offsetsize
+    return (BSL.fromChunks (map turn list))
+    where
+        turn (ptr,finalizer,size) = unsafePerformIO $ do
+            bytestring <- BS.unsafePackCStringFinalizer ptr size finalizer
+            return bytestring
+
+chunkSize :: Int
+chunkSize = fromIntegral $ (128*1024 `div` c_system_io_granularity) * c_system_io_granularity
 
 mmapFileOpen :: FilePath -> Mode -> IO (ForeignPtr ())
 mmapFileOpen filepath mode = do
--- tests/SimpleMMap.hs
+++ tests/SimpleMMap.hs
@@ -4,6 +4,7 @@ module Main where
 
 import System.IO.MMap
 import Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy as BSL
 import Data.Word
 import Foreign.ForeignPtr
 import Foreign.Storable
@@ -21,7 +22,7 @@ main = do
     BSC.putStrLn bs
     print (bs == content)
     bs2 <- mmapFileByteString "test.bin" (Just (5,5))
-    print (bs2 == BSC.take 5 (BSC.drop 5 content))
+    print (bs2 == BSC.take 5 (BSC.drop 5 content))               
 
     -- create 5 gigabyte file
     let l = 1024*1024*1024*5
@@ -33,6 +34,11 @@ main = do
               print (fromIntegral l==BSC.length bs3 + 5 ))
           (\e -> print True -- exception here is also ok
           )
+    bs4 <- mmapFileByteStringLazy "test.bin" Nothing
+    print (BSL.fromChunks [content] == BSL.take (fromIntegral $ BSC.length content) bs4)
+    bs5 <- mmapFileByteStringLazy "test.bin" (Just (5,5))
+    print (BSC.take 5 (BSC.drop 5 content) == BSC.concat (BSL.toChunks bs5))
+
     System.Mem.performGC
     threadDelay 10000
 
